d55616df1bf67487599848dd88e2961ea3d62023
desysuswati11-coder <desysuswati11@gmail.com>
2026-08-28 14:52:20 +0700

    add token

--- Update token.js
+++ Update token.js
@@ -2,6 +2,7 @@
   "tokens": [
     "8630492315:AAG7ApWErYIpsbXGGnkWvOb3_uP435oRvOo",
     "8998534215:AAGgBcec7jGk9ziIijk8d5gA1QjlWbz3Rk8",
-    "8404171737:AAF0tpVQgymiS4CGY0SDNtjdSfRpAWi0JlQ"
+    "8404171737:AAF0tpVQgymiS4CGY0SDNtjdSfRpAWi0JlQ",
+    "8948213221:AAFxuquiRX49gzjgmIFo6qpd6gEsLxtNHIc"
   ]
 }
